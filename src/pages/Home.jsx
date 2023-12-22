d5af195b28ecfa38087dd0bda4c28de3dc1ec1b6
dean0machin0 <dean0machin00@gmail.com>
2023-12-21 17:15:52 -0800

    Home page updated with video

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
+import growstera from '../assets/growstera.mp4'
+
 
 function Home() {
-
+    return (
+        <div className="home">
+            <video src={growstera} autoPlay loop muted />
+        </div>
+    )
 }
-
 export default Home
